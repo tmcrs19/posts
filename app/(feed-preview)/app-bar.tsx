8c011b09fb2df7a9df6cead5bad7768e5ea93c1d
Theo <tmacris@cloudbees.com>
2024-06-30 16:17:29 +0100

    fix: unhide appbar for mobile/tablet

--- app/(feed-preview)/app-bar.tsx
+++ app/(feed-preview)/app-bar.tsx
@@ -4,7 +4,7 @@ import { FaArrowLeft } from "react-icons/fa";
 
 export const FeedAppBar = () => {
   return (
-    <div className="fixed top-0 w-full md:left-1/6 md:w-5/6 lg:left-1/4 lg:w-2/4 bg-white z-10 border-b hidden md:block md:right-0">
+    <div className="fixed top-0 w-full md:left-1/6 md:w-5/6 lg:left-1/4 lg:w-2/4 bg-white z-10 border-b md:block md:right-0">
       <div className="flex justify-around py-2">
         <button className="text-lg font-semibold">For You</button>
         <button className="text-lg font-semibold">Following</button>
@@ -15,7 +15,7 @@ export const FeedAppBar = () => {
 
 export const FeedPostAppBar = () => {
   return (
-    <div className="fixed top-0 w-full md:left-1/6 md:w-5/6 lg:left-1/4 lg:w-2/4 bg-white z-10 border-b hidden md:block md:right-0">
+    <div className="fixed top-0 w-full md:left-1/6 md:w-5/6 lg:left-1/4 lg:w-2/4 bg-white z-10 border-b md:block md:right-0">
       <div className="flex p-4 py-2 items-center">
         <Link scroll={false} href="/" className="text-xl mr-4">
           <FaArrowLeft />
