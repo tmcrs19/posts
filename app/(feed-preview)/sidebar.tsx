8f9cff79883a102b3ee047e16a69f747c2b0c4f0
Theo <tmacris@cloudbees.com>
2024-06-30 11:57:18 +0100

    fix: sidebar content width

--- app/(feed-preview)/sidebar.tsx
+++ app/(feed-preview)/sidebar.tsx
@@ -4,7 +4,9 @@ import React from "react";
 const Sidebar = () => {
   return (
     <aside className="fixed p-4 right-0 top-0 h-screen w-full md:w-1/3 lg:w-1/4 bg-gray-200 hidden lg:block">
-      <Trends />
+      <div className="w-4/5 mr-auto">
+        <Trends />
+      </div>
     </aside>
   );
 };
